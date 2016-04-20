152fb59fcd6aaa09eab369622373fdf460b6f15e
Ubuntu <ubuntu@ip-172-31-57-239.ec2.internal>
2016-04-20 20:52:36 +0000

    codificacion variables

--- frappe/templates/includes/login/login.js
+++ frappe/templates/includes/login/login.js
@@ -49,53 +49,48 @@ window.disable_signup = {{ disable_signup and "true" or "false" }};
 login.route = function() {
 
 var route = window.location.hash.slice(1);
-        
         var routeLogin = route.split('/');
-        
         routeLog = routeLogin[0];
         console.log(routeLog);
         if (!routeLog){
-            routeLog = "login";
-        } 
-        login[routeLog]();
+routeLog = "login";
+        }
+login[routeLog]();
 }
 
 login.login = function() {
-    $("form").toggle(false);
-    $(".form-login").toggle(true);
+$("form").toggle(false);
+        $(".form-login").toggle(true);
 }
 
 login.integracion = function(){
-    
-    
-    
-    var rutaSitio = window.location.hash.slice(1);
+
+var rutaSitio = window.location.hash.slice(1);
+        var variables = rutaSitio.split('/');
+        console.log(variables);
+        var formulario = variables[0];
+        console.log(formulario);
+        var parametrosUsuario = variables[1];
         
-    var variables = rutaSitio.split('/');
-    console.log(variables);
-    var formulario = variables[0];
-    console.log(formulario);
-    var parametros = variables[1];
-    console.log(parametros);
-    alert("Ingreso a integracion OK");
-    return false;
-    
-    var rutaVer = window.location.hash;
-    var rutaVer2 = window.location.hash.slice(2);
-    console.log(rutaVer);
-    console.log(rutaVer2);
-    
-    event.preventDefault();
-    var args = {};
-    args.cmd = "login";
-    args.usr = ($("#login_email").val() || "").trim();
-    args.pwd = $("#login_password").val();
-    args.device = "desktop";
-    if (!args.usr || !args.pwd) {
-            frappe.msgprint(__("Both login and password required"));
+        var varUs = decode64(parametrosUsuario);
+        console.log(varUs);
+        alert("Ingreso a integracion OK");
+        return false;
+        var rutaVer = window.location.hash;
+        var rutaVer2 = window.location.hash.slice(2);
+        console.log(rutaVer);
+        console.log(rutaVer2);
+        event.preventDefault();
+        var args = {};
+        args.cmd = "login";
+        args.usr = ($("#login_email").val() || "").trim();
+        args.pwd = $("#login_password").val();
+        args.device = "desktop";
+        if (!args.usr || !args.pwd) {
+frappe.msgprint(__("Both login and password required"));
         return false;
         }
-        login.call(args);
+login.call(args);
         return false;
 }
 
@@ -173,3 +168,73 @@ frappe.msgprint(data.message);
         $(".form-signup, .form-forgot").removeClass("hide");
                 $(document).trigger('login_rendered');
         });
+        var keyStr = "ABCDEFGHIJKLMNOP" +
+        "QRSTUVWXYZabcdef" +
+        "ghijklmnopqrstuv" +
+        "wxyz0123456789+/" +
+        "=";
+        function encode64(input) {
+        input = escape(input);
+                var output = "";
+                var chr1, chr2, chr3 = "";
+                var enc1, enc2, enc3, enc4 = "";
+                var i = 0;
+                do {
+                chr1 = input.charCodeAt(i++);
+                        chr2 = input.charCodeAt(i++);
+                        chr3 = input.charCodeAt(i++);
+                        enc1 = chr1 >> 2;
+                        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                        enc4 = chr3 & 63;
+                        if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                enc4 = 64;
+                }
+
+                output = output +
+                        keyStr.charAt(enc1) +
+                        keyStr.charAt(enc2) +
+                        keyStr.charAt(enc3) +
+                        keyStr.charAt(enc4);
+                        chr1 = chr2 = chr3 = "";
+                        enc1 = enc2 = enc3 = enc4 = "";
+                } while (i < input.length);
+                return output;
+        }
+
+function decode64(input) {
+var output = "";
+        var chr1, chr2, chr3 = "";
+        var enc1, enc2, enc3, enc4 = "";
+        var i = 0;
+        // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+        var base64test = /[^A-Za-z0-9\+\/\=]/g;
+        if (base64test.exec(input)) {
+alert("There were invalid base64 characters in the input text.\n" +
+        "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+        "Expect errors in decoding.");
+}
+input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+        do {
+        enc1 = keyStr.indexOf(input.charAt(i++));
+                enc2 = keyStr.indexOf(input.charAt(i++));
+                enc3 = keyStr.indexOf(input.charAt(i++));
+                enc4 = keyStr.indexOf(input.charAt(i++));
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+                output = output + String.fromCharCode(chr1);
+                if (enc3 != 64) {
+        output = output + String.fromCharCode(chr2);
+        }
+        if (enc4 != 64) {
+        output = output + String.fromCharCode(chr3);
+        }
+
+        chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+        } while (i < input.length);
+        return unescape(output);
+}
