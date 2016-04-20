09d27a86df70d48b80333731a36734c2cc2c462b
Ubuntu <ubuntu@ip-172-31-57-239.ec2.internal>
2016-04-20 19:32:49 +0000

    variable

--- frappe/templates/includes/login/login.js
+++ frappe/templates/includes/login/login.js
@@ -62,6 +62,8 @@ login.login = function() {
 	$("form").toggle(false);
 	$(".form-login").toggle(true);
         
+        console.log(get_url_arg());;
+        
         alert($("#login_email").val());
         alert($("#login_password").val())
 }
