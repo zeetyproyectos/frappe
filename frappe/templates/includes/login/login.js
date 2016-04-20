dcf76140195074eb6559bc68394df33bfd1ad9bb
Ubuntu <ubuntu@ip-172-31-57-239.ec2.internal>
2016-04-20 20:27:24 +0000

    Validar integracion

--- frappe/templates/includes/login/login.js
+++ frappe/templates/includes/login/login.js
@@ -1,151 +1,157 @@
 window.disable_signup = {{ disable_signup and "true" or "false" }};
-
-
-window.login = {};
-
-login.bind_events = function() {
-	$(window).on("hashchange", function() {
-		login.route();
-	});
-
-	$(".form-login").on("submit", function(event) {
-		event.preventDefault();
-		var args = {};
-		args.cmd = "login";
-		args.usr = ($("#login_email").val() || "").trim();                
-		args.pwd = $("#login_password").val();
-		args.device = "desktop";
-		if(!args.usr || !args.pwd) {
-			frappe.msgprint(__("Both login and password required"));
-			return false;
-		}
-		login.call(args);
-		return false;
-	});
-
-	$(".form-signup").on("submit", function(event) {
-		event.preventDefault();
-		var args = {};
-		args.cmd = "frappe.core.doctype.user.user.sign_up";
-		args.email = ($("#signup_email").val() || "").trim();
-		args.full_name = ($("#signup_fullname").val() || "").trim();
-		if(!args.email || !valid_email(args.email) || !args.full_name) {
-			frappe.msgprint(__("Valid email and name required"));
-			return false;
-		}
-		login.call(args);
-		return false;
-	});
-
-	$(".form-forgot").on("submit", function(event) {
-		event.preventDefault();
-		var args = {};
-		args.cmd = "frappe.core.doctype.user.user.reset_password";
-		args.user = ($("#forgot_email").val() || "").trim();
-		if(!args.user) {
-			frappe.msgprint(__("Valid Login id required."));
-			return false;
-		}
-		login.call(args);
-		return false;
-	});
-}
+        window.login = {};
+        login.bind_events = function() {
+        $(window).on("hashchange", function() {
+        login.route();
+        });
+                $(".form-login").on("submit", function(event) {
+        event.preventDefault();
+                var args = {};
+                args.cmd = "login";
+                args.usr = ($("#login_email").val() || "").trim();
+                args.pwd = $("#login_password").val();
+                args.device = "desktop";
+                if (!args.usr || !args.pwd) {
+        frappe.msgprint(__("Both login and password required"));
+                return false;
+        }
+        login.call(args);
+                return false;
+        });
+                $(".form-signup").on("submit", function(event) {
+        event.preventDefault();
+                var args = {};
+                args.cmd = "frappe.core.doctype.user.user.sign_up";
+                args.email = ($("#signup_email").val() || "").trim();
+                args.full_name = ($("#signup_fullname").val() || "").trim();
+                if (!args.email || !valid_email(args.email) || !args.full_name) {
+        frappe.msgprint(__("Valid email and name required"));
+                return false;
+        }
+        login.call(args);
+                return false;
+        });
+                $(".form-forgot").on("submit", function(event) {
+        event.preventDefault();
+                var args = {};
+                args.cmd = "frappe.core.doctype.user.user.reset_password";
+                args.user = ($("#forgot_email").val() || "").trim();
+                if (!args.user) {
+        frappe.msgprint(__("Valid Login id required."));
+                return false;
+        }
+        login.call(args);
+                return false;
+        });
+        }
 
 
 login.route = function() {
-        var rutaVer = window.location.hash;
-        var rutaVer2 = window.location.hash.slice(2);
-    
-        console.log(rutaVer);
-        console.log(rutaVer2); 
-    
-	var route = window.location.hash.slice(1);
-	if(!route) route = "login";
-	login[route]();
+
+var route = window.location.hash.slice(1);
+        if (!route) route = "login";
+        login[route]();
 }
 
 login.login = function() {
-	$("form").toggle(false);
-	$(".form-login").toggle(true);
-        
-        
+    $("form").toggle(false);
+    $(".form-login").toggle(true);
+}
+
+login.integracion = function(){
+    
+    alert("Ingreso a integracion OK");
+    return false;
+    var rutaVer = window.location.hash;
+    var rutaVer2 = window.location.hash.slice(2);
+    console.log(rutaVer);
+    console.log(rutaVer2);
+    
+    event.preventDefault();
+    var args = {};
+    args.cmd = "login";
+    args.usr = ($("#login_email").val() || "").trim();
+    args.pwd = $("#login_password").val();
+    args.device = "desktop";
+    if (!args.usr || !args.pwd) {
+            frappe.msgprint(__("Both login and password required"));
+        return false;
+        }
+        login.call(args);
+        return false;
 }
 
 login.forgot = function() {
-	$("form").toggle(false);
-	$(".form-forgot").toggle(true);
+$("form").toggle(false);
+        $(".form-forgot").toggle(true);
 }
 
 login.signup = function() {
-	$("form").toggle(false);
-	$(".form-signup").toggle(true);
+$("form").toggle(false);
+        $(".form-signup").toggle(true);
 }
 
 
 // Login
 login.call = function(args) {
-	frappe.freeze();
-
-	$.ajax({
-		type: "POST",
-		url: "/",
-		data: args,
-		dataType: "json",
-		statusCode: login.login_handlers
-	}).always(function(){
-		frappe.unfreeze();
-	});
+frappe.freeze();
+        $.ajax({
+        type: "POST",
+                url: "/",
+                data: args,
+                dataType: "json",
+                statusCode: login.login_handlers
+        }).always(function(){
+frappe.unfreeze();
+        });
 }
 
 login.login_handlers = (function() {
-	var get_error_handler = function(default_message) {
-		return function(xhr, data) {
-			if(xhr.responseJSON) {
-				data = xhr.responseJSON;
-			}
-			var message = data._server_messages
-				? JSON.parse(data._server_messages).join("\n") : default_message;
-			frappe.msgprint(message);
-		};
-	}
+var get_error_handler = function(default_message) {
+return function(xhr, data) {
+if (xhr.responseJSON) {
+data = xhr.responseJSON;
+        }
+var message = data._server_messages
+        ? JSON.parse(data._server_messages).join("\n") : default_message;
+        frappe.msgprint(message);
+        };
+        }
 
-	var login_handlers = {
-		200: function(data) {
-			if(data.message=="Logged In") {
-				window.location.href = get_url_arg("redirect-to") || "/desk";
-			} else if(data.message=="No App") {
-				if(localStorage) {
-					var last_visited =
-						localStorage.getItem("last_visited")
-						|| get_url_arg("redirect-to");
-					localStorage.removeItem("last_visited");
-				}
+var login_handlers = {
+200: function(data) {
+if (data.message == "Logged In") {
+window.location.href = get_url_arg("redirect-to") || "/desk";
+        } else if (data.message == "No App") {
+if (localStorage) {
+var last_visited =
+        localStorage.getItem("last_visited")
+        || get_url_arg("redirect-to");
+        localStorage.removeItem("last_visited");
+        }
 
-				if(last_visited && last_visited != "/login") {
-					window.location.href = last_visited;
-				} else {
-					window.location.href = "/me";
-				}
-			} else if(["#signup", "#forgot"].indexOf(window.location.hash)!==-1) {
-				frappe.msgprint(data.message);
-			}
-		},
-		401: get_error_handler(__("Invalid Login")),
-		417: get_error_handler(__("Oops! Something went wrong"))
-	};
-
-	return login_handlers;
+if (last_visited && last_visited != "/login") {
+window.location.href = last_visited;
+        } else {
+window.location.href = "/me";
+        }
+} else if (["#signup", "#forgot"].indexOf(window.location.hash) !== - 1) {
+frappe.msgprint(data.message);
+        }
+},
+        401: get_error_handler(__("Invalid Login")),
+        417: get_error_handler(__("Oops! Something went wrong"))
+        };
+        return login_handlers;
 })();
+        frappe.ready(function() {
+        login.bind_events();
+                if (!window.location.hash) {
+        window.location.hash = "#login";
+        } else {
+        $(window).trigger("hashchange");
+        }
 
-frappe.ready(function() {
-	login.bind_events();
-
-	if (!window.location.hash) {
-		window.location.hash = "#login";
-	} else {
-		$(window).trigger("hashchange");
-	}
-
-	$(".form-signup, .form-forgot").removeClass("hide");
-	$(document).trigger('login_rendered');
-});
+        $(".form-signup, .form-forgot").removeClass("hide");
+                $(document).trigger('login_rendered');
+        });
